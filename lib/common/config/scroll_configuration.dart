77be2046286fc4ba439243c0709b92c2d6679e18
AndreaR <andrea.rubeis@nttdata.com>
2020-12-17 09:39:59 +0100

    Add app scroll configuration

---
+++ lib/common/config/scroll_configuration.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomScrollBehaviour extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) =>
+      BouncingScrollPhysics();
+}
